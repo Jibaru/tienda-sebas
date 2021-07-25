d5973e358ede14bea0939e483e6f8af9a4ea8f04
Ignacio <ignacioruedaboada@hotmail.com>
2021-07-25 01:08:57 -0500

    DTO

---
+++ src/main/java/com/untels/dto/auth/EmailClaveDTO.java
@@ -0,0 +1,26 @@
+package com.untels.dto.auth;
+
+public class EmailClaveDTO {
+    String email;
+    String clave;
+
+    public EmailClaveDTO() {
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getClave() {
+        return this.clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+}
---
+++ src/main/java/com/untels/dto/auth/UsuarioCompletoDTO.java
@@ -0,0 +1,53 @@
+package com.untels.dto.auth;
+
+public class UsuarioCompletoDTO {
+    String nombre;
+    String apellidoPaterno;
+    String apellidoMaterno;
+    String email;
+    String clave;
+
+    public UsuarioCompletoDTO() {
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return this.apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return this.apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getClave() {
+        return this.clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+}
