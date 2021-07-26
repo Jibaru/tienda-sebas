29b9cef712d2672b2c31863f6086138b7214d5b5
Cegaje <Cegaje17@gmail.com>
2021-07-26 18:26:22 -0500

    gestion ventas

--- src/main/java/com/untels/enums/TipoComprobante.java
+++ src/main/java/com/untels/enums/TipoComprobante.java
@@ -1,5 +1,18 @@
 package com.untels.enums;
 
 public enum TipoComprobante {
-    BOLETA, FACTURA
+    BOLETA, FACTURA;
+
+    public static TipoComprobante fromString(String tipoComprobante) {
+        TipoComprobante enuma = null;
+        switch (tipoComprobante.toUpperCase()) {
+            case "BOLETA":
+                enuma = BOLETA;
+                break;
+            case "FACTURA":
+                enuma = FACTURA;
+                break;
+        }
+        return enuma;
+    }
 }
