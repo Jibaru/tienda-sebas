514d94be496873d30fde9ecf9b40491fa1e2afa7
Jibaru <ignacioruedaboada@hotmail.com>
2021-07-26 22:33:59 -0500

    SQL pruebas

---
+++ sql_pruebas.sql
@@ -0,0 +1,106 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
+--
+-- Host: localhost    Database: tiendasebas
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `articulo`
+--
+
+LOCK TABLES `articulo` WRITE;
+/*!40000 ALTER TABLE `articulo` DISABLE KEYS */;
+INSERT INTO `articulo` VALUES (1,'0001','Ajustable al instante Tecnología de compresión doble para de la cintura. Ayuda a obtener cintura y una figura más delgada. Apoya la parte posterior para el levantamiento de peso',_binary '','https://cdn.lumingo.com/medias/0100468640-000000000004593500-0-c515Wx515H?context=bWFzdGVyfGltYWdlc3w2ODg5fGltYWdlL2pwZWd8aW1hZ2VzL2hlNS9oNGEvODk5Mzg4Mzc1MDQzMC5qcGd8YWU2Njc3MmM4ZTQ3NjA4NDBhODAyYTgyNGFhOTQ2ZGY1ODA2YjZlMzI5NGU1OTRjZjlkNmI5YTQyMWY3ZmE1Yg','Faja reductora',15,30,6),(2,'0002','Diseño exclusivo con tres agarres. Recubierto en caucho. Seguro, poco impacto y mayor duración',_binary '\0','https://cdn.lumingo.com/medias/0100468640-000000000004582570-0-c515Wx515H?context=bWFzdGVyfGltYWdlc3wzNDE4NHxpbWFnZS9qcGVnfGltYWdlcy9oNzkvaDBhLzg5NzIxMDc1MTM4ODYuanBnfGNmMjdmOTdhOTc0N2QzN2Q3OTU5YTY0M2VkMDI5NTcxMjhkNTRlMDE1YzdkYWIwMGI0MmJlOTM3NTM0ODc5ODE','Discos de pesas 15kg',540,50,1),(3,'003','Doble Rueda Diámetro de rueda: 20 cm Desarmable Incluye Pad para rodillas Mejora tus abdominales Color transparente',_binary '\0','https://cdn.lumingo.com/medias/0100547696-000000000004811695-0-c515Wx515H?context=bWFzdGVyfGltYWdlc3wyNzIzMXxpbWFnZS9qcGVnfGltYWdlcy9oNzAvaDk3LzkzNTc0MTgzNjQ5NTguanBnfGIyYjcwNDljMGExY2FhYmRmODNkMmEyNGNiYjIwZGIzOThhNGQ5MWNlZjBlODU4MmJiNDZjMDhmNTgxODQ5Njc','Rueda Abdominal Doble Astmart Muscle Ab Roller Desarmable',54,49,1),(4,'0012','elota de Pvc de Alto Calibre Pelota deportiva Para yoga y Pilates Tamaño: 75cm Durable y Resistente No Viene Inflada',_binary '\0','https://cdn.lumingo.com/medias/0100487688-000000000004580528-0-c515Wx515H?context=bWFzdGVyfGltYWdlc3wyOTYyN3xpbWFnZS9qcGVnfGltYWdlcy9oZDAvaDIwLzg5NjgwNjY3NjA3MzQuanBnfDYyZTM4OGZmZWQ1NzI1NGNmNWUzYTFjMmVkNDI0ZGY3ZGU5NWU5OTMzOGU0MTQwYWFhNTIwYjA1Yzg1NzI5YWE','Pelota de pilates',45,133,4),(5,'1AFB','Producto elaborado con diversas fuentes proteicas de alta calidad.Ideal para personas que deseen mantener o subir su peso (periodo de volumen), tanto para hombres, mujeres, atletas de alto rendimiento físico, entre otros.',_binary '','https://farmacityar.vteximg.com.br/arquivos/ids/159524-1000-1000/24287_suplemento-dietario-amino-4500-x-150-un_imagen-1.jpg?v=636670315089170000','Suplemento UN',60,100,3),(6,'002F','Es un aparato sencillo que se basa en una rueda con un rodamiento central que hace que gire y dos agarres, uno a cada lado, que nos permitirá que podamos agarrarnos con cada una de las manos o de los pies.',_binary '','http://s3.amazonaws.com/imagenes-sellers-mercado-ripley/2021/01/18214536/3-3.jpg','Rueda Abdominal',1532.1,40,1),(7,'5FF5','Para saltos con pie junto, saltos consecutivos, spikking, etc. Ùtil para rutinas de preparación física.',_binary '','https://i.linio.com/p/308efc8b84f6c617f27336d5e793aaa5-product.jpg','Cono y Varilla',80.5,80,2),(8,'562F','Pieza de equipamiento utilizada en el entrenamiento con pesas, es un tipo de peso libre. Pueden utilizarse individualmente o por parejas (una en cada mano).',_binary '','https://www.mideporte.pe/wp-content/uploads/2019/11/MANCUERNAS-NEOPRENE-AZUL-3KG.jpg','Mancuernas',230,30,1),(9,'85FT','Herramienta útil que puedes usar en el gimnasio de muchas formas diferentes. Los ejercicios con pelota de estabilidad desafían tu equilibrio y fuerzan al cuerpo a hacer trabajar los músculos del abdomen.',_binary '','http://2.bp.blogspot.com/-wzy8lMwAoPg/T-my_XKrz1I/AAAAAAAABoU/cpaeOdRbzhE/s1600/513N0EDXN5L._SL500_AA300_.jpg','Balon Suizo',70,40,4),(10,'9012','Te ayudará a ser más velóz. Trabajar tus piernas con una sesión de salto de cuerda, mejorará tu cadencia y zancada.',_binary '\0','https://images.utilex.pe/055920/450x450/salta-soga-con-mango-de-madera-CYJRSOIQLRVGI.jpeg','Salta Soga',15,100,6),(11,'5T2F','La barra para pesas cromada servirá para trabajar los bíceps, se considera un entrenamiento muy eficaz para el desarrollo del músculo. Es la mejor para la construcción de músculo y fuerza, porque el agarre con la palma hacia afuera hace que sus bíceps se ',_binary '','http://s3.amazonaws.com/imagenes-sellers-mercado-ripley/2020/09/10125302/barra-rectaa.jpg','Barra recta de acero cromado',75,14,1),(12,'774G','La barra curva y con forma de Z es una barra diseñada para entrenar la parte superior de los brazos, especialmente los bíceps. Tiene forma de “w”, de zigzag, de ahí que también se le denomine barra w. La forma curva permite un agarre cómodo, incluso para ',_binary '','https://http2.mlstatic.com/D_NQ_NP_992382-MPE44710950151_012021-O.jpg','Barra Z Olimpica',60,15,1),(13,'KILK','Permite trabajar la fuerza, la estabilidad, la velocidad y la elasticidad en un mismo entrenamiento y normalmente los deportistas continúan después con las pesas.',_binary '','https://sportfitness.com.pe/223-superlarge_default/set-de-5-bandas-juego-de-5x-banda-elastica.jpg','Banda elastica',20,45,6);
+/*!40000 ALTER TABLE `articulo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `categoria`
+--
+
+LOCK TABLES `categoria` WRITE;
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+INSERT INTO `categoria` VALUES (1,'Pieza de metal de peso conocido que se usa para determinar lo que pesa un objeto, con la que se equilibra en una balanza.',_binary '','Pesas'),(2,'Máquinas y bancas para ejercicios comunes',_binary '','Máquinas y bancas'),(3,'Pastillas, Bebidas y Energéticos en general',_binary '','Suplementos Deportivos'),(4,'Balones deportivos o para ejercicio en general',_binary '','Balones'),(5,'Rodilleras de todo tipo',_binary '','Rodilleras'),(6,'Sogas y Fajas en general',_binary '','Sogas y Fajas');
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `detalle_ingreso`
+--
+
+LOCK TABLES `detalle_ingreso` WRITE;
+/*!40000 ALTER TABLE `detalle_ingreso` DISABLE KEYS */;
+/*!40000 ALTER TABLE `detalle_ingreso` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `detalle_venta`
+--
+
+LOCK TABLES `detalle_venta` WRITE;
+/*!40000 ALTER TABLE `detalle_venta` DISABLE KEYS */;
+INSERT INTO `detalle_venta` VALUES (1,4,0,216,3,1);
+/*!40000 ALTER TABLE `detalle_venta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `ingreso`
+--
+
+LOCK TABLES `ingreso` WRITE;
+/*!40000 ALTER TABLE `ingreso` DISABLE KEYS */;
+INSERT INTO `ingreso` VALUES (1,_binary '','2021-07-26 22:23:32.924000',0,'90121','001','BOLETA',145,1,1);
+/*!40000 ALTER TABLE `ingreso` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `persona`
+--
+
+LOCK TABLES `persona` WRITE;
+/*!40000 ALTER TABLE `persona` DISABLE KEYS */;
+INSERT INTO `persona` VALUES (1,'Martinez','Salas',NULL,'Nando',NULL,NULL,NULL,'CLIENTE',1),(2,'Sánchez','Romero','Av. Los Corales 123 Surco','Adrian','09123194','901231401','DNI','CLIENTE',2),(3,'Tapia','Rodríguez','Av. Gonzales Prada 920 La Victoria','Humberto','0931001201','901231231','RUC','PROVEEDOR',3),(4,'Anth','Cena',NULL,'John',NULL,NULL,NULL,'CLIENTE',4);
+/*!40000 ALTER TABLE `persona` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'$2a$10$oTHj6Jt6WzLfmXm1SMUJuuihVgJEoB4kzjsKaS/b/OGLpm8xeZf.6','admin@untels.edu.pe',_binary '','ADMIN'),(2,'$2a$10$V5vW4.f3h25baXxiACyB1exGxgneYs.pB2H7j6IVsybfAQPVdIyye','adrian2001@gmail.com',_binary '\0','NORMAL'),(3,'$2a$10$b/ryoa/GzbNG5tg9pcmVueR6F2sIecf7RFfszb2tCavdIV.eg/IKa','humberto1985@progym.pe',_binary '\0','NORMAL'),(4,'$2a$10$2TZmsK5teI0d5Mq0wnWNUuJkIScC2gRqFV/X22V167W.hA2JOQbyq','john_cena_2001@gmail.com',_binary '','NORMAL');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `venta`
+--
+
+LOCK TABLES `venta` WRITE;
+/*!40000 ALTER TABLE `venta` DISABLE KEYS */;
+INSERT INTO `venta` VALUES (1,'COMPLETADO','2021-07-26 17:03:14.735000',1,'-437610449','0001','BOLETA',216,1,NULL);
+/*!40000 ALTER TABLE `venta` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-07-26 22:29:28
